70c040512d12cfc9c166f6c1946047390e834b9f
Khash <khashayar.m.003@gmail.com>
2022-09-12 09:53:27 +0430

    hide or unhide

--- js/script.js
+++ js/script.js
@@ -1,20 +1,26 @@
 //Selecting Elements and Global Variables
 const name = document.querySelector("input[type='text']").focus();
 const jobRoll = document.getElementById('title');
-const otherJobRoll = document.getElementById('other-job-role').style.display = 'none';
+const otherJobRoll = document.getElementById('other-job-role');
 
 // console.log(jobRoll)
 // console.log(otherJobRoll)
-//event 'change' for jobroll to listen for otherjob roll
 
+//hide the otherJobRoll on page load.
+otherJobRoll.style.display = 'none';
+
+//event to hide and unhide the "other text box" on change.
 jobRoll.addEventListener('change', (e) => {
     const otherJR = e.target.value;
-    console.log(otherJR);
+    // console.log(otherJR);
     if (otherJR === 'other' ){
+        otherJobRoll.style.transition = 'all 0.5s ease-in-out';
         otherJobRoll.style.display = 'block';
+        
+    } else {
+        otherJobRoll.style.display = 'none';
     }
-   
-})
+});
 
 
 
