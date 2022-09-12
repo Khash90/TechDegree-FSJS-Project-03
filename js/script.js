17d4b743b756808fdc61e7621d92b072e2bb883d
Khash <khashayar.m.003@gmail.com>
2022-09-12 09:41:37 +0430

    hide other

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
 //Selecting Elements and Global Variables
 const name = document.querySelector("input[type='text']").focus();
+const jobRoll = document.getElementById('title');
+const otherJobRoll = document.getElementById('other-job-role').style.display = 'none';
 
+// console.log(jobRoll)
+// console.log(otherJobRoll)
+//event 'change' for jobroll to listen for otherjob roll
+
+jobRoll.addEventListener('change', (e) => {
+    const otherJR = e.target.value;
+    console.log(otherJR);
+    if (otherJR === 'other' ){
+        otherJobRoll.style.display = 'block';
+    }
+   
+})
 
 
 
